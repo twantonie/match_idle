6bfa5d2d1bd4f81d3bb7b97683a6a8580f44b929
Twan Spil <t.spil@chronoptics.com>
2022-10-02 13:29:07 +1300

    Update green chest color

--- tests/window_recognition.cpp
+++ tests/window_recognition.cpp
@@ -1,5 +1,6 @@
 #include <fmt/core.h>
 
+#include <boost/filesystem.hpp>
 #include <catch2/catch.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <window_recognition.hpp>
@@ -12,7 +13,8 @@ constexpr std::array file_names{
     "treasure_hunt_1.png",  "treasure_hunt_2.png",  "treasure_hunt_3.png",
     "treasure_hunt_4.png",  "treasure_hunt_5.png",  "treasure_hunt_6.png",
     "treasure_hunt_7.png",  "treasure_hunt_8.png",  "treasure_hunt_9.png",
-    "treasure_hunt_10.png", "treasure_hunt_11.png", "treasure_hunt_12.png"};
+    "treasure_hunt_10.png", "treasure_hunt_11.png", "treasure_hunt_12.png",
+    "treasure_hunt_13.png"};
 
 // clang-format off
 std::array<std::vector<mi::Piece>, file_names.size()> boards{
@@ -136,6 +138,16 @@ Board{
   {Type::BrownChest}, {Type::Copper}, {Type::Silver}, {Type::GreenChest}, {Type::Sack}, {Type::Silver}, {Type::Gold}, {Type::BrownChest},
   {Type::Silver}, {Type::RedChest}, {Type::GreenChest}, {Type::BrownChest}, {Type::RedChest}, {Type::Copper}, {Type::Copper}, {Type::Sack},
 },
+Board{
+  {Type::Silver}, {Type::Copper}, {Type::Gold}, {Type::Copper}, {Type::Silver}, {Type::Silver}, {Type::Gold}, {Type::Copper},
+  {Type::Gold}, {Type::Sack}, {Type::Silver}, {Type::Sack}, {Type::Gold}, {Type::Sack}, {Type::Silver}, {Type::Gold},
+  {Type::Gold}, {Type::Gold}, {Type::Sack}, {Type::Gold}, {Type::Gold}, {Type::Sack}, {Type::Sack}, {Type::Copper},
+  {Type::Sack}, {Type::Copper}, {Type::Copper}, {Type::Gold}, {Type::GreenChest}, {Type::Copper}, {Type::BrownChest}, {Type::Silver},
+  {Type::Copper}, {Type::Silver}, {Type::RedChest}, {Type::BrownChest}, {Type::Silver}, {Type::Copper}, {Type::Gold}, {Type::Gold},
+  {Type::BrownChest}, {Type::RedChest}, {Type::Silver}, {Type::GreenChest}, {Type::RedChest}, {Type::Silver}, {Type::Silver}, {Type::Copper},
+  {Type::Gold}, {Type::Silver}, {Type::Copper}, {Type::Vault}, {Type::BrownChest}, {Type::GreenChest}, {Type::Sack}, {Type::GreenChest},
+  {Type::GreenChest}, {Type::Gold}, {Type::Sack}, {Type::GreenChest}, {Type::Vault}, {Type::Gold}, {Type::Gold}, {Type::GreenChest},
+},
 };
 // clang-format on
 
@@ -145,19 +157,23 @@ TEST_CASE("read board") {
   file_location += "/";
   file_location += file_names[loc];
 
-  auto window = cv::imread(file_location);
-  auto board = cheat::read_board(window);
+  if (boost::filesystem::is_regular_file(file_location)) {
+    auto window = cv::imread(file_location);
+    auto board = cheat::read_board(window);
 
-  if (!boards[loc].empty()) {
-    CHECK(board == boards[loc]);
+    if (!boards[loc].empty()) {
+      CHECK(board == boards[loc]);
+    } else {
+      static constexpr mi::GridLayout grid{8, 8};
+      fmt::print("{}\n", loc);
+      mi::print_board(board, grid);
+
+      auto it = std::find_if(
+          board.begin(), board.end(),
+          [](const mi::Piece &piece) { return piece.type == Type::Empty; });
+      CHECK(it == board.end());
+    }
   } else {
-    static constexpr mi::GridLayout grid{8, 8};
-    fmt::print("{}\n", loc);
-    mi::print_board(board, grid);
-
-    auto it = std::find_if(
-        board.begin(), board.end(),
-        [](const mi::Piece &piece) { return piece.type == Type::Empty; });
-    CHECK(it == board.end());
+    FAIL_CHECK();
   }
 }
